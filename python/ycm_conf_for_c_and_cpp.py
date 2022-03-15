8be7e86ba6c197c0409e5d2a10646f8301cffe8a
FooFooDamon <udc577@126.com>
2022-03-15 10:20:34 +0800

    python/ycm_conf_for_c_and_cpp.py: Add description and changelog.

--- python/ycm_conf_for_c_and_cpp.py
+++ python/ycm_conf_for_c_and_cpp.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+#-*-coding: utf-8-*-
+
+#
+# Configuration script of YouCompleteMe VIM plugin,
+# which defines common rules for C/C++ grammar checking and code completion.
+#
+# Copyright 2022 Man Hung-Coeng <udc577@126.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import os
 
 flags = [
@@ -15,4 +37,21 @@ SOURCE_EXTENSIONS = [ ".c", ".C", ".cc", ".cpp", ".cxx", ".c++" ]
 
 def FlagsForFile(filename, **kwargs):
     return { "flags": flags, "do_cache": True }
+
+if __name__ == "__main__":
+    import sys
+    print('*** DO NOT run this script directly!', file = sys.stderr)
+    print('Import it into the ".ycm_extra_conf.py" file in your project instead.', file = sys.stderr)
+
+#
+# ================
+#   CHANGE LOG
+# ================
+#
+# >>> 2022-02-03, Man Hung-Coeng:
+#   01. Create.
+#
+# >>> 2022-03-15, Man Hung-Coeng:
+#   01. Add description and changelog.
+#
 

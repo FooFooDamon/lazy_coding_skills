2c2b24914723823e06f83d7c557dc7589095984a
FooFooDamon <udc577@126.com>
2023-10-22 16:09:19 +0800

    python/ycm_conf_for_*.py: Support the new working mode based on clangd.

--- python/ycm_conf_for_c_and_cpp.py
+++ python/ycm_conf_for_c_and_cpp.py
@@ -5,7 +5,7 @@
 # Configuration script of YouCompleteMe VIM plugin,
 # which defines common rules for C/C++ grammar checking and code completion.
 #
-# Copyright 2022-2023 Man Hung-Coeng <udc577@126.com>
+# Copyright (c) 2022-2023 Man Hung-Coeng <udc577@126.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -35,14 +35,99 @@ for inc_dir in os.popen("echo | $(g++ --print-prog-name=cc1plus) -v 2>&1 | grep 
 
 SOURCE_EXTENSIONS = [ ".c", ".C", ".cc", ".cpp", ".cxx", ".c++" ]
 
+MAX_SRC_FILES = 50000
+
+#
+# For the old working mode based on libclang.
+#
 def FlagsForFile(filename, **kwargs):
+#{#
     return { "flags": flags, "do_cache": True }
+#}#
+
+def FindNearestTargetFromBottomUp(start_dir: str, filename: str):
+#{#
+    dest_dir = start_dir
+
+    while '/' != dest_dir and '' != dest_dir and not os.path.exists(os.path.join(dest_dir, filename)):
+        dest_dir = os.path.dirname(dest_dir)
+
+    return None if ('/' == dest_dir or '' == dest_dir) else os.path.join(dest_dir, filename)
+#}#
+
+def MakeCommandDictItem(dirpath: str, filename: str):
+#{#
+    cmd_dict = { "directory": dirpath, "file": filename, "arguments": [ "g++" ] }
+    cmd_dict["arguments"].extend(flags)
+    cmd_dict["arguments"].extend([ "-c", "-o", os.path.splitext(cmd_dict["file"])[0] + ".o" ])
+    cmd_dict["arguments"].append(cmd_dict["file"])
+
+    return cmd_dict
+#}#
+
+def CreateCommandsJsonIfNone(current_file: str):
+#{#
+    if os.path.splitext(current_file)[1] not in SOURCE_EXTENSIONS:
+        return
+
+    this_dir, this_file = os.path.split(current_file)
+    ycm_file = FindNearestTargetFromBottomUp(this_dir, ".ycm_extra_conf.py")
+
+    if None == ycm_file or None != FindNearestTargetFromBottomUp(this_dir, "compile_commands.json"):
+        return
+
+    import json
+
+    src_count = 0
+    cmd_json = []
+    json_dir = os.path.dirname(ycm_file)
+    json_file = os.path.join(json_dir, "compile_commands.json")
+
+    for root, dirs, files in os.walk(json_dir, followlinks = True):
+    #{#
+        for f in files:
+        #{#
+            if os.path.splitext(f)[1] not in SOURCE_EXTENSIONS:
+                continue
+
+            cmd_json.append(MakeCommandDictItem(root, f))
+
+            src_count += 1
+            if src_count >= MAX_SRC_FILES:
+                break
+        #}#
+
+        if src_count >= MAX_SRC_FILES:
+            break
+    #}# for os.walk(json_dir)
+
+    with open(json_file, "w") as fp:
+        fp.write(json.dumps(cmd_json, indent = 4, ensure_ascii = False))
+        print("Created: " + json_file)
+
+    if src_count >= MAX_SRC_FILES:
+    #{#
+        raise RuntimeWarning(
+            "*** " + json_file + ": Too many source files, only the first " + str(src_count) + " were chosen!!!"
+        )
+    #}#
+#}# def CreateCommandsJsonIfNone()
+
+#
+# For the new working mode based on clangd.
+#
+def Settings(**kwargs):
+#{#
+    CreateCommandsJsonIfNone(kwargs["filename"])
+    return { "flags": flags }
+#}#
 
 if __name__ == "__main__":
+#{#
     import sys
     print('*** DO NOT run this script directly!', file = sys.stderr)
     print('\nUsage example 1: Make a symbolic link:', file = sys.stderr)
-    print('    $ ln -s ' + os.path.abspath(__file__) + ' /path/to/your/directory/.ycm_extra_conf.py', file = sys.stderr)
+    print('    $ ln -s ' + os.path.abspath(__file__) + ' /path/to/your/.ycm_extra_conf.py', file = sys.stderr)
     print('\nUsage example 2: Create your own .ycm_extra_conf.py '
         + 'and input (at least) the following lines into it:', file = sys.stderr)
     print('    import os, sys', file = sys.stderr)
@@ -50,19 +135,23 @@ if __name__ == "__main__":
     print('    sys.path.append("' + os.path.abspath(os.path.dirname(__file__)) + '")', file = sys.stderr)
     print('    from ' + os.path.splitext(os.path.basename(__file__))[0] + ' import *', file = sys.stderr)
     print('    # flags.extend([ "-I", YCM_CONF_DIR ]) # More directories and macros if needed.', file = sys.stderr)
+#}#
 
 #
 # ================
 #   CHANGE LOG
 # ================
 #
-# >>> 2022-02-03, Man Hung-Coeng:
+# >>> 2022-02-03, Man Hung-Coeng <udc577@126.com>:
 #   01. Create.
 #
-# >>> 2022-03-15, Man Hung-Coeng:
+# >>> 2022-03-15, Man Hung-Coeng <udc577@126.com>:
 #   01. Add description and changelog.
 #
-# >>> 2023-10-06, Man Hung-Coeng:
+# >>> 2023-10-06, Man Hung-Coeng <udc577@126.com>:
 #   01. Improve the usage guide to make it more descriptive.
+#
+# >>> 2023-10-22, Man Hung-Coeng <udc577@126.com>:
+#   01. Support the new working mode based on clangd.
 #
 
--- python/ycm_conf_for_linux_driver.py
+++ python/ycm_conf_for_linux_driver.py
@@ -30,6 +30,7 @@ flags = [
     "-Wall"
     , "-std=gnu11"
     , "-x", "c"
+    , "-nostdinc"
     #, "-I", THIS_DIR
     , "-I", os.path.join(THIS_DIR, "..", "c_and_cpp", "native")
     , "-I", os.path.join(KERNEL_ROOT, "arch", ARCH, "include")
@@ -50,10 +51,95 @@ flags = [
 
 SOURCE_EXTENSIONS = [ ".c" ]
 
+MAX_SRC_FILES = 50000
+
+#
+# For the old working mode based on libclang.
+#
 def FlagsForFile(filename, **kwargs):
+#{#
     return { "flags": flags, "do_cache": True }
+#}#
+
+def FindNearestTargetFromBottomUp(start_dir: str, filename: str):
+#{#
+    dest_dir = start_dir
+
+    while '/' != dest_dir and '' != dest_dir and not os.path.exists(os.path.join(dest_dir, filename)):
+        dest_dir = os.path.dirname(dest_dir)
+
+    return None if ('/' == dest_dir or '' == dest_dir) else os.path.join(dest_dir, filename)
+#}#
+
+def MakeCommandDictItem(dirpath: str, filename: str):
+#{#
+    cmd_dict = { "directory": dirpath, "file": filename, "arguments": [ "g++" ] }
+    cmd_dict["arguments"].extend(flags)
+    cmd_dict["arguments"].extend([ "-c", "-o", os.path.splitext(cmd_dict["file"])[0] + ".o" ])
+    cmd_dict["arguments"].append(cmd_dict["file"])
+
+    return cmd_dict
+#}#
+
+def CreateCommandsJsonIfNone(current_file: str):
+#{#
+    if os.path.splitext(current_file)[1] not in SOURCE_EXTENSIONS:
+        return
+
+    this_dir, this_file = os.path.split(current_file)
+    ycm_file = FindNearestTargetFromBottomUp(this_dir, ".ycm_extra_conf.py")
+
+    if None == ycm_file or None != FindNearestTargetFromBottomUp(this_dir, "compile_commands.json"):
+        return
+
+    import json
+
+    src_count = 0
+    cmd_json = []
+    json_dir = os.path.dirname(ycm_file)
+    json_file = os.path.join(json_dir, "compile_commands.json")
+
+    for root, dirs, files in os.walk(json_dir, followlinks = True):
+    #{#
+        for f in files:
+        #{#
+            if os.path.splitext(f)[1] not in SOURCE_EXTENSIONS:
+                continue
+
+            cmd_json.append(MakeCommandDictItem(root, f))
+
+            src_count += 1
+            if src_count >= MAX_SRC_FILES:
+                break
+        #}#
+
+        if src_count >= MAX_SRC_FILES:
+            break
+    #}# for os.walk(json_dir)
+
+    with open(json_file, "w") as fp:
+        fp.write(json.dumps(cmd_json, indent = 4, ensure_ascii = False))
+        print("Created: " + json_file)
+
+    if src_count >= MAX_SRC_FILES:
+    #{#
+        raise RuntimeWarning(
+            "*** " + json_file + ": Too many source files, only the first " + str(src_count) + " were chosen!!!"
+        )
+    #}#
+#}# def CreateCommandsJsonIfNone()
+
+#
+# For the new working mode based on clangd.
+#
+def Settings(**kwargs):
+#{#
+    CreateCommandsJsonIfNone(kwargs["filename"])
+    return { "flags": flags }
+#}#
 
 if __name__ == "__main__":
+#{#
     import sys
     print('*** DO NOT run this script directly!', file = sys.stderr)
     print('\nUsage example 1: Make a symbolic link:', file = sys.stderr)
@@ -65,6 +151,7 @@ if __name__ == "__main__":
     print('    sys.path.append("' + os.path.abspath(os.path.dirname(__file__)) + '")', file = sys.stderr)
     print('    from ' + os.path.splitext(os.path.basename(__file__))[0] + ' import *', file = sys.stderr)
     print('    # flags.extend([ "-I", YCM_CONF_DIR ]) # More directories and macros if needed.', file = sys.stderr)
+#}#
 
 #
 # ================
@@ -77,5 +164,8 @@ if __name__ == "__main__":
 # >>> 2023-10-06, Man Hung-Coeng <udc577@126.com>:
 #   01. Add KBUILD_MODNAME macro option.
 #   02. Improve the usage guide.
+#
+# >>> 2023-10-22, Man Hung-Coeng <udc577@126.com>:
+#   01. Support the new working mode based on clangd.
 #
 

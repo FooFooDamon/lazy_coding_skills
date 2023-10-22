41773e5cb166e864d2981b4ed633e16deee98761
FooFooDamon <udc577@126.com>
2023-10-22 16:10:28 +0800

    c_and_cpp/native/.ycm_extra_conf.py: Add macro COMMPROTO_LITTLE_ENDIAN and KBUILD_MODNAME.

--- c_and_cpp/native/.ycm_extra_conf.py
+++ c_and_cpp/native/.ycm_extra_conf.py
@@ -2,5 +2,5 @@ import os, sys
 YCM_CONF_DIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(YCM_CONF_DIR, "../../python"))
 from ycm_conf_for_c_and_cpp import *
-flags.extend([ "-I", YCM_CONF_DIR ])
+flags.extend([ "-I", YCM_CONF_DIR, "-D", "COMMPROTO_LITTLE_ENDIAN", '-D', 'KBUILD_MODNAME="<none>"' ])
 

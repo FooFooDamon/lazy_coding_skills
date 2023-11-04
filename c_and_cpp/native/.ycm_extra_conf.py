59b821c715a86eedf46dc2ff5b5f931a4d987024
FooFooDamon <udc577@126.com>
2023-11-04 18:27:46 +0800

    c_and_cpp/native/.ycm_extra_conf.py: Add support for parsing Linux driver code files.

--- c_and_cpp/native/.ycm_extra_conf.py
+++ c_and_cpp/native/.ycm_extra_conf.py
@@ -1,6 +1,27 @@
 import os, sys
 YCM_CONF_DIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(YCM_CONF_DIR, "../../python"))
-from ycm_conf_for_c_and_cpp import *
-flags.extend([ "-I", YCM_CONF_DIR, "-D", "COMMPROTO_LITTLE_ENDIAN", '-D', 'KBUILD_MODNAME="<none>"' ])
+import ycm_conf_for_c_and_cpp
+from ycm_conf_for_linux_driver import flags as driver_flags
+
+ycm_conf_for_c_and_cpp.flags.extend([ "-D", "COMMPROTO_LITTLE_ENDIAN", "-D", "TEST" ])
+driver_flags.extend([ "-D", "TEST" ])
+
+DRIVER_SRC_BASENAMES = [
+    "chardev_group",
+    "evol_kernel",
+    "klogging",
+]
+
+def FlagsForFile(filename: str, **kwargs):
+#{#
+    base_name = os.path.splitext(os.path.basename(filename))[0]
+    app_flags = ycm_conf_for_c_and_cpp.flags
+
+    return { "flags": driver_flags if base_name in DRIVER_SRC_BASENAMES else app_flags }
+#}#
+
+ycm_conf_for_c_and_cpp.FlagsForFile = FlagsForFile
+
+Settings = ycm_conf_for_c_and_cpp.Settings
 

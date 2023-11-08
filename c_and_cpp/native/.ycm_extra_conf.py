de66fe6cdaa5e62037684cbaa45240edbc4f3186
FooFooDamon <udc577@126.com>
2023-11-08 10:18:24 +0800

    c_and_cpp/native/.ycm_extra_conf.py: Add YCM_CONF_DIR/c_and_cpp/native to the list of -I options.

--- c_and_cpp/native/.ycm_extra_conf.py
+++ c_and_cpp/native/.ycm_extra_conf.py
@@ -5,18 +5,24 @@ sys.path.append(os.path.join(YCM_CONF_DIR, "python"))
 import ycm_conf_for_c_and_cpp
 from ycm_conf_for_linux_driver import flags as driver_flags
 
-ycm_conf_for_c_and_cpp.flags.extend([ "-D", "COMMPROTO_LITTLE_ENDIAN", "-D", "TEST" ])
-driver_flags.extend([ "-D", "TEST" ])
+extra_inc_dirs = []
+if os.path.exists(os.path.join(YCM_CONF_DIR, "c_and_cpp")):
+#{#
+    extra_inc_dirs.append("-I" + os.path.join(YCM_CONF_DIR, "c_and_cpp", "native"))
+#}#
 if os.path.exists(os.path.join(YCM_CONF_DIR, ".paths")):
 #{#
-    extra_inc_dirs = [
+    extra_inc_dirs.extend([
         "-I" + os.path.join(YCM_CONF_DIR, ".paths", i, "c_and_cpp", "native")
         for i in os.listdir(os.path.join(YCM_CONF_DIR, ".paths"))
-    ]
-
-    ycm_conf_for_c_and_cpp.flags.extend(extra_inc_dirs)
-    driver_flags.extend(extra_inc_dirs)
+    ])
 #}#
+
+ycm_conf_for_c_and_cpp.flags.extend([ "-D", "COMMPROTO_LITTLE_ENDIAN", "-D", "TEST" ])
+ycm_conf_for_c_and_cpp.flags.extend(extra_inc_dirs)
+
+driver_flags.extend([ "-D", "TEST" ])
+driver_flags.extend(extra_inc_dirs)
 
 DRIVER_SRC_BASENAMES = [
     "chardev_group",
